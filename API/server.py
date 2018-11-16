a9c222ad7132b203d87271525d942e5d44a9d6ab
Paul Bricman <paubric@gmail.com>
2018-11-16 18:24:57 +0200

    Update server.py

--- API/server.py
+++ API/server.py
@@ -1,7 +1,9 @@
 from flask import Flask # Flask
 from Terrabyte import get, prepare # our functions
+from flask_cors import CORS
 
 app = Flask(__name__) # init flask
+CORS(app)
 
 @app.route('/') # home roune
 def index():
